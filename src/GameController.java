95a8a56ffa2e1d945d8644c5c5c05004404a9346
Tyler <tylersyme12@gmail.com>
2019-10-26 14:50:37 -0400

    Added all images to canvas

--- src/GameController.java
+++ src/GameController.java
@@ -15,6 +15,20 @@ public class GameController extends GraphicsProgram {
     private static final int IMG_SIDE = 60;
     private static final int IMG_OFFSET = (SIDE - IMG_SIDE) / 2;
 
+    // Images from https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent
+    private static final String whiteBishop = "images/Chess_blt60.png";
+    private static final String blackBishop = "images/Chess_bdt60.png";
+    private static final String whiteKing = "images/Chess_klt60.png";
+    private static final String blackKing = "images/Chess_kdt60.png";
+    private static final String whiteKnight = "images/Chess_nlt60.png";
+    private static final String blackKnight = "images/Chess_ndt60.png";
+    private static final String whitePawn = "images/Chess_plt60.png";
+    private static final String blackPawn = "images/Chess_pdt60.png";
+    private static final String whiteQueen = "images/Chess_qlt60.png";
+    private static final String blackQueen = "images/Chess_qdt60.png";
+    private static final String whiteRook = "images/Chess_rlt60.png";
+    private static final String blackRook = "images/Chess_rdt60.png";
+
     private void createBoard() {
 
         this.resize(WIDTH, HEIGHT);
@@ -39,41 +53,65 @@ public class GameController extends GraphicsProgram {
 
         for (int i = 0; i < SQUARES_PER_SIDE; i++) {
             for (int j = 0; j < SQUARES_PER_SIDE; j++) {
-                if (board.getBoard()[i + SQUARES_PER_SIDE * j] != null) {
-                    switch (board.getBoard()[i + SQUARES_PER_SIDE * j].getClass().getSimpleName()) {
-                        case "Bishop":
-                            if (board.getBoard()[i + SQUARES_PER_SIDE * j].getTeam()) { // white bishop
-                                GImage wBishop = new GImage("images/Chess_blt60.png", i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                if (board.getBoard()[i + SQUARES_PER_SIDE * j] != null) { // if there's a piece
+
+                    if (board.getBoard()[i + SQUARES_PER_SIDE * j].getTeam()) { // white team
+
+                        switch (board.getBoard()[i + SQUARES_PER_SIDE * j].getClass().getSimpleName()) {
+                            case "Bishop":
+                                GImage wBishop = new GImage(whiteBishop, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
                                 add(wBishop);
-                            } else { // black bishop
-                                GImage bBishop = new GImage("images/Chess_bdt60.png", i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
-                                add(bBishop);
-                            }
-                            break;
-                        case "King":
-                            if (board.getBoard()[i + SQUARES_PER_SIDE * j].getTeam()) { // white king
-                                GImage wKing = new GImage("images/Chess_klt60.png", i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                break;
+                            case "King":
+                                GImage wKing = new GImage(whiteKing, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
                                 add(wKing);
-                            } else { // black king
-                                GImage bKing = new GImage("images/Chess_kdt60.png", i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                break;
+                            case "Knight":
+                                GImage wKnight = new GImage(whiteKnight, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(wKnight);
+                                break;
+                            case "Pawn":
+                                GImage wPawn = new GImage(whitePawn, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(wPawn);
+                                break;
+                            case "Queen":
+                                GImage wQueen = new GImage(whiteQueen, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(wQueen);
+                                break;
+                            case "Rook":
+                                GImage wRook = new GImage(whiteRook, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(wRook);
+                                break;
+                        }
+
+                    } else { // black team
+
+                        switch (board.getBoard()[i + SQUARES_PER_SIDE * j].getClass().getSimpleName()) {
+                            case "Bishop":
+                                GImage bBishop = new GImage(blackBishop, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(bBishop);
+                                break;
+                            case "King":
+                                GImage bKing = new GImage(blackKing, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
                                 add(bKing);
-                            }
-                            break;
-                        case "Knight":
-
-                            break;
-
-                        case "Pawn":
-
-                            break;
-
-                        case "Queen":
-
-                            break;
-
-                        case "Rook":
-
-                            break;
+                                break;
+                            case "Knight":
+                                GImage bKnight = new GImage(blackKnight, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(bKnight);
+                                break;
+                            case "Pawn":
+                                GImage bPawn = new GImage(blackPawn, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(bPawn);
+                                break;
+                            case "Queen":
+                                GImage bQueen = new GImage(blackQueen, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(bQueen);
+                                break;
+                            case "Rook":
+                                GImage bRook = new GImage(blackRook, i * SIDE + IMG_OFFSET, j * SIDE + IMG_OFFSET);
+                                add(bRook);
+                                break;
+                        }
 
                     }
                 }
